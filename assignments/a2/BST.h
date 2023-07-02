94f06bf824bde6f7b19212d0f7dc2ef90b77585d
GracePearcey <pearcey.grace@gmail.com>
2023-07-02 17:11:56 -0230

    a2 complete

--- assignments/a2/BST.h
+++ assignments/a2/BST.h
@@ -15,19 +15,19 @@
  */
 
 #include <cassert>
-#include <cstddef>      // for definition of size_t
-#include <functional>   // std::function
-#include <memory>       // std::unique_ptr
-#include <stack>        // std::stack
+#include <cstddef>	  // for definition of size_t
+#include <functional> // std::function
+#include <memory>	  // std::unique_ptr
+#include <stack>	  // std::stack
 #include <iostream>
 
 using namespace std;
 
-template<typename T, typename Comparator = std::less<T>>
+template <typename T, typename Comparator = std::less<T>>
 class BinarySearchTree
 {
 public:
-    struct Node; //forward declaration
+	struct Node; // forward declaration
 	enum class Traversal
 	{
 		PreOrder,
@@ -67,116 +67,192 @@ public:
 		 *
 		 * This operator method just calls private methods to try and
 		 * keep the logic of the various traversal mechanisms clear.
-		 */   
-		Iterator operator ++ (int){
-			cout << "blue" << endl;
-            if (this->order_ == Traversal::PreOrder){
+		 */
+		Iterator operator++(int)
+		{
+			if (this->order_ == Traversal::PreOrder)
+			{
 				this->PreOrder();
-            }
-            else if (this->order_ == Traversal::InOrder){
-
-            }
-            else if (this->order_ == Traversal::PostOrder){
-
-            }
-            return *this;
-        }
+			}
+			else if (this->order_ == Traversal::InOrder)
+			{
+				this->InOrder();
+			}
+			else if (this->order_ == Traversal::PostOrder)
+			{
+				this->PostOrder();
+			}
+			return *this;
+		}
 
 		//! Dereference the iterator at its current position
-		const T& operator * (){
-            return this->node_pointer_->element_;
-        }
+		const T &operator*()
+		{
+			return this->current_node_->element_;
+		}
 
 		//! Is this iterator *not* the same as another?
-		bool operator != (const Iterator &other){
-			if (this->iterator_node_ == other.iterator_node_){
-				return false;
+		bool operator!=(const Iterator &other)
+		{
+			if (this->current_node_ != other.current_node_)
+			{
+				return true; //node is not the same
 			}
-			return true;
+			return false; //node is the same
 		}
 
 		// add whatever else you need here
-        std::stack<Node*> nodes_;
-        Node* node_pointer_;
-		Node* iterator_node_;
-        Traversal order_;
+		std::stack<Node *> nodes_;
+		Node *node_pointer_;
+		Node *current_node_;
+		Node *previous_node_;
+		Traversal order_;
 
-    private:
-        void PreOrder(){
+	private:
+		/**
+		 * PreOrder Traversal of tree
+		*/
+		void PreOrder()
+		{
 			bool node_incremented = false;
-			cout << "grapes" << endl;
-			while (!node_incremented){
-				if(nodes_.empty() == true && node_pointer_ == nullptr){
-					//we've iterated through the entire tree
-					cout << "finished iterating" << endl;
-					iterator_node_ = nullptr;
+
+			while (!node_incremented)
+			{
+				if (nodes_.empty() == true && node_pointer_ == nullptr)
+				{	// finished iterating through the tree
+					current_node_ = nullptr; // set to null so it matches the end() iterator
 					return;
 				}
 
-				if (node_pointer_ != nullptr){
-					node_incremented = true;
-					iterator_node_ = node_pointer_;
-					nodes_.push(node_pointer_);
-					node_pointer_ = node_pointer_->left_.get();
+				if (node_pointer_ != nullptr)
+				{
+					// increment the current_node
+					node_incremented = true; // lets us exit while loop
+					current_node_ = node_pointer_;
+					nodes_.push(node_pointer_); //add the node to the stack
+					node_pointer_ = node_pointer_->left_.get(); // move to this node's left child
 				}
-				else {
-					cout << "zebra" << endl;
-					Node* prev_node = nodes_.top();
-					cout << "apple" << endl;
-					nodes_.pop(); 
-					cout << "banana" << endl;
-					node_pointer_ = prev_node->right_.get();
-					cout << "carrot" << endl;
+				else
+				{
+					previous_node_ = nodes_.top(); // previous node is on top of the stack
+					nodes_.pop(); //remove the previous node
+					node_pointer_ = previous_node_->right_.get(); // move to the previous node's right child
 				}
 			}
-			printStack(nodes_);
 			return;
 		}
 
-		void printStack(std::stack<Node*> node_stack){
-			cout << "printing stack" << endl;
-			while ( node_stack.empty() == false){
-				cout << node_stack.top()->element_<< endl;
-				node_stack.pop();
+		/**
+		 * In Order Tree Traversal
+		*/
+		void InOrder()
+		{
+			bool node_incremented = false;
+			while (!node_incremented)
+			{
+				if (nodes_.empty() == true && node_pointer_ == nullptr)
+				{
+					// finished iterating through the tree
+					current_node_ = nullptr; // set to null so it matches the end() iterator
+					return;
+				}
+
+				if (node_pointer_ != nullptr)
+				{
+					// move to the left child
+					nodes_.push(node_pointer_);
+					node_pointer_ = node_pointer_->left_.get();
+				}
+				else
+				{
+					previous_node_ = nodes_.top();
+					current_node_ = nodes_.top(); // set the current node for iterator to point to
+					node_incremented = true; // lets us exit while loop 
+					nodes_.pop();
+					node_pointer_ = previous_node_->right_.get(); //move to the right child for next time
+				}
 			}
-			cout << "end of stack " << endl;
+			return;
 		}
 
-        
+		void PostOrder()
+		{
+			bool node_incremented = false;
+			while (!node_incremented)
+			{
+				if (nodes_.empty() == true && node_pointer_ == nullptr)
+				{ // finished iterating through the tree
+					current_node_ = nullptr; // set to null so it matches the end() iterator
+					return;
+				}
 
+				if (node_pointer_ != nullptr)
+				{
+					// move to the left child
+					nodes_.push(node_pointer_);
+					node_pointer_ = node_pointer_->left_.get();
+				}
+				else
+				{
+					current_node_ = nodes_.top();
+					if (current_node_->right_ != nullptr and current_node_->right_.get() != previous_node_)
+					{	
+						// current node has a right child we haven't seen yet
+						node_pointer_ = current_node_->right_.get();
+					}
+					else
+					{
+						// we have found the next node
+						node_incremented = true; // lets us exit while loop
+						previous_node_ = current_node_;
+						nodes_.pop();
 
+					}
+				}
+			}
+			return;
+		}
 	};
 
-    /**
-     * Returns an iterator that can be used to traverse the tree in the given order.
-     *
-     * This iterator should visit every node in the tree exactly once, after which
-     * it should test equal to the iterator returned from `end()`.
-     */
-	Iterator begin(Traversal order){
-        Iterator iter;
-        iter.order_ = order;
-        if (order == Traversal::PreOrder){
-            iter.node_pointer_ = this->root_.get();
-			iter.iterator_node_ = this->root_.get();
-        }
-        else if (order == Traversal::InOrder){
-
-        }
-		return iter;
-    }
-    /**
-     * The end of a tree traversal.
-     *
-     * The iterator returned by this method should be usable as the end-of-iteration
-     * marker for any iterator on this tree, whether it was traversing the tree in
-     * pre-, in- or post-order.
-     */
-	Iterator end(){
+	/**
+	 * Returns an iterator that can be used to traverse the tree in the given order.
+	 *
+	 * This iterator should visit every node in the tree exactly once, after which
+	 * it should test equal to the iterator returned from `end()`.
+	 */
+	Iterator begin(Traversal order)
+	{
 		Iterator iter;
-		iter.iterator_node_ = nullptr;
-        return iter;
-    }
+		iter.order_ = order;
+		if (order == Traversal::PreOrder)
+		{
+			iter.node_pointer_ = this->root_.get();
+			iter.current_node_ = this->root_.get();
+		}
+		else if (order == Traversal::InOrder)
+		{
+			iter.node_pointer_ = this->root_.get();
+		}
+		else if (order == Traversal::PostOrder)
+		{
+			iter.node_pointer_ = this->root_.get();
+		}
+		iter++;
+		return iter;
+	}
+	/**
+	 * The end of a tree traversal.
+	 *
+	 * The iterator returned by this method should be usable as the end-of-iteration
+	 * marker for any iterator on this tree, whether it was traversing the tree in
+	 * pre-, in- or post-order.
+	 */
+	Iterator end()
+	{
+		Iterator iter;
+		iter.current_node_ = nullptr;
+		return iter;
+	}
 
 public:
 	struct Node
@@ -217,10 +293,6 @@ public:
 
 				right_->dot(o);
 			}
-
-			//Node* get(){
-			//	return 
-			//}
 		}
 
 		T element_;
--- assignments/a2/bst_test.cc
+++ assignments/a2/bst_test.cc
@@ -3,31 +3,179 @@
 
 using namespace std;
 
+/* Checking Preorder with integers*/
+
+//int main(){
+//	// Create a tree of integers:
+//	BinarySearchTree<int> t;
+//
+//	// Fancy way of not having to write BinarySearchTree<int> twice:
+//	const auto Order = decltype(t)::Traversal::PreOrder;
+//
+//	//// Insert the values 4, 2, 6, 1, 3, 5 and 7:
+//	for (int n : { 4, 2, 6, 1, 3, 5, 7 })
+//	{
+//		t.insert(n);
+//	}
+//
+//	//// The following should print: 1234567
+//	for (auto i = t.begin(Order); i != t.end(); i++)
+//	{
+//		cout << *i << " ";
+//	}
+//	cout << "\n" ;
+//};
+//
+
+/* Checking Preorder with unique pointers */
+
+//int main() {
+//
+//	// Create a tree of integers:
+//	BinarySearchTree<unique_ptr<int>> t;
+//	// Fancy way of not having to write BinarySearchTree<int> twice:
+//	const auto Order = decltype(t)::Traversal::PreOrder;
+//	//// Insert the values 4, 2, 6, 1, 3, 5 and 7:
+//
+//	std::unique_ptr<int> ptr0(new int(9));
+//	std::unique_ptr<int> ptr1(new int(3));
+//	std::unique_ptr<int> ptr2(new int(4));
+//	std::unique_ptr<int> ptr3(new int(0));
+//	std::unique_ptr<int> ptr4(new int(1));
+//	std::unique_ptr<int> ptr5(new int(32));
+//	std::unique_ptr<int> ptr6(new int(17));
+//
+//	t.insert(std::move(ptr3));
+//	t.insert(std::move(ptr5));
+//	t.insert(std::move(ptr2));
+//	t.insert(std::move(ptr4));
+//	t.insert(std::move(ptr6));
+//	t.insert(std::move(ptr1));
+//	t.insert(std::move(ptr0));
+//
+//
+//	//// The following should print: 1234567
+//	for (auto i = t.begin(Order); i != t.end(); i++)
+//	{
+//		cout << *(*i) << " ";
+//	}
+//	cout << "\n" ;
+//
+//	return 0;
+//
+//};
+
+
+
+/* Checking InOrder Traversal*/
+/*
 int main(){
-	// Create a tree of integers:
+
+
 	BinarySearchTree<int> t;
 
-	// Fancy way of not having to write BinarySearchTree<int> twice:
-	const auto Order = decltype(t)::Traversal::PreOrder;
-//
-	//// Insert the values 4, 2, 6, 1, 3, 5 and 7:
+	const auto Order = decltype(t)::Traversal::InOrder;
+
 	for (int n : { 4, 2, 6, 1, 3, 5, 7 })
 	{
 		t.insert(n);
 	}
-//
-	//// The following should print: 1234567
-	int count = 0;
+
 	for (auto i = t.begin(Order); i != t.end(); i++)
 	{
-		if (i.iterator_node_ == nullptr){
-			cout << "i is null" << endl;
-		}
 		cout << *i << " ";
-		//cout << "count: " << count << endl;
-		count++;
+	}
+	cout << "\n" ;
+	
+
+	BinarySearchTree<unique_ptr<int>> t1;
+
+	const auto Order1 = decltype(t1)::Traversal::InOrder;
+
+	std::unique_ptr<int> ptr0(new int(9));
+	std::unique_ptr<int> ptr1(new int(3));
+	std::unique_ptr<int> ptr2(new int(4));
+	std::unique_ptr<int> ptr3(new int(0));
+	std::unique_ptr<int> ptr4(new int(1));
+	std::unique_ptr<int> ptr5(new int(32));
+	std::unique_ptr<int> ptr6(new int(17));
+	t1.insert(std::move(ptr3));
+	t1.insert(std::move(ptr5));
+	t1.insert(std::move(ptr2));
+	t1.insert(std::move(ptr4));
+	t1.insert(std::move(ptr6));
+	t1.insert(std::move(ptr1));
+	t1.insert(std::move(ptr0));
+
+	for (auto i = t1.begin(Order1); i != t1.end(); i++)
+	{
+		cout << *(*i) << " ";
+	}
+	cout << "\n" ;
+	return 0;
+}
+*/
+
+/* Checking PostOrder Traversal*/
+
+int main(){
+
+
+	BinarySearchTree<int> t;
+
+	const auto Order = decltype(t)::Traversal::PostOrder;
+
+	for (int n : { 4, 2, 6, 1, 3, 5, 7 })
+	{
+		t.insert(n);
+	}
+
+	for (auto i = t.begin(Order); i != t.end(); i++)
+	{
+		cout << *i << " ";
 	}
 	cout << "\n" ;
 
-};
+	BinarySearchTree<int> t1;
+
+	const auto Order1 = decltype(t1)::Traversal::PostOrder;
+
+	for (int n : { 3, 2, 1, 0, 5, 4, 6 })
+	{
+		t1.insert(n);
+	}
+
+	for (auto j = t1.begin(Order1); j != t1.end(); j++)
+	{
+		cout << *j << " ";
+	}
+	cout << "\n" ;
+	
+
+	//BinarySearchTree<unique_ptr<int>> t1;
+//
+	//const auto Order1 = decltype(t1)::Traversal::InOrder;
+//
+	//std::unique_ptr<int> ptr0(new int(9));
+	//std::unique_ptr<int> ptr1(new int(3));
+	//std::unique_ptr<int> ptr2(new int(4));
+	//std::unique_ptr<int> ptr3(new int(0));
+	//std::unique_ptr<int> ptr4(new int(1));
+	//std::unique_ptr<int> ptr5(new int(32));
+	//std::unique_ptr<int> ptr6(new int(17));
+	//t1.insert(std::move(ptr3));
+	//t1.insert(std::move(ptr5));
+	//t1.insert(std::move(ptr2));
+	//t1.insert(std::move(ptr4));
+	//t1.insert(std::move(ptr6));
+	//t1.insert(std::move(ptr1));
+	//t1.insert(std::move(ptr0));
+//
+	//for (auto i = t1.begin(Order1); i != t1.end(); i++)
+	//{
+	//	cout << *(*i) << " ";
+	//}
+	//cout << "\n" ;
+	return 0;
+}
 
