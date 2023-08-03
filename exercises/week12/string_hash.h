fb4ac1ae0af52d8c2c8b1e01a496774c780f7523
GracePearcey <pearcey.grace@gmail.com>
2023-08-03 12:52:16 -0230

    finished july31 exercises

--- exercises/week12/string_hash.h
+++ exercises/week12/string_hash.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 #include <cstring>
-#define CAPACITY 50000 // Size of the HashTable.
+#define CAPACITY 50 // Size of the HashTable.
 
 using namespace std;
 
@@ -26,12 +26,40 @@ private:
         int count;
     };
 
-    unsigned long hash_function(char* str)
+    /**
+     * Takes the sum of ascii values for each character in word, then modulo CAPACITY
+    */
+    unsigned long hash_function1(char* str)
     {
         unsigned long i = 0;
 
         for (int j = 0; str[j]; j++) //continues until it reaches the null-terminator character '\0'
             i += str[j];
+
+        return i % CAPACITY;
+    }
+
+    /**
+     * Take the ascii character of the first character in the word, then modulo CAPACITY
+    */
+    unsigned long hash_function2(char* str)
+    {
+        unsigned long i = 0;
+
+        i = str[0];
+
+        return i % CAPACITY;
+    }
+
+    /**
+     * Takes the numbers of characters in the word, then modulo CAPACITY
+    */
+    unsigned long hash_function3(char* str)
+    {
+        unsigned long i = 0;
+
+        for (int j = 0; str[j]; j++) //continues until it reaches the null-terminator character '\0'
+            i++;
 
         return i % CAPACITY;
     }
@@ -74,6 +102,8 @@ public:
 
         for (int i = 0; i < table->size; i++)
             table->items[i] = NULL;
+
+        table->overflow_buckets.resize(CAPACITY);
     }
 
     void addItem(char* key, char* value)
@@ -85,7 +115,7 @@ public:
         strcpy(item->key, key);
         strcpy(item->value, value);
 
-        int index = hash_function(key);
+        int index = hash_function3(key);
 
         Item* current_item = this->table->items[index];
 
@@ -125,10 +155,42 @@ public:
             if (this->table->items[i])
             {
                 printf("Index:%d, Key:%s, Value:%s\n", i, this->table->items[i] -> key, this->table->items[i]->value);
+                if (!this->table->overflow_buckets[i].empty())
+                {
+                    for (auto j: this->table->overflow_buckets[i])
+                    {
+                        printf("Index:%d, Key:%s, Value:%s\n", i, j-> key, j->value);
+                    }
+                }
             }
+
+            
+
+
         }
     
         printf("-------------------\n\n");
+    }
+
+    void printDistribution()
+    {
+        for(int i=0; i<this->table->size; i++)
+        {
+            int count = 0;
+            if (this->table->items[i])
+            {
+                count++;
+                if (!this->table->overflow_buckets[i].empty())
+                {
+                    for (auto j: this->table->overflow_buckets[i])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            printf("%d,%d\n",i,count);
+        }
     }
 
 private:
--- exercises/week12/string_hash_test.cpp
+++ exercises/week12/string_hash_test.cpp
@@ -5,18 +5,29 @@ int main()
 
     HashTable h;
 
-    h.addItem( (char *)"Grace", (char *)"Cedar Brae Crescent");
-    h.addItem( (char *)"Madison", (char *)"Cypress Street");
-    h.addItem( (char *)"Michael", (char *)"Elm Street");
-    h.addItem( (char *)"Raymond", (char *)"Blackstrap Path");
-    h.addItem( (char *)"James", (char *)"Indean Meal Line");
-    h.addItem( (char *)"Casey", (char *)"Larkhall Street");
-    h.addItem( (char *)"Grace", (char *)"Outer Battery Road");
+    h.addItem( (char *)"grace", (char *)"Cedar Brae Crescent");
+    h.addItem( (char *)"rachel", (char *)"Cypress Street");
+    h.addItem( (char *)"maria", (char *)"Elm Street");
+    h.addItem( (char *)"luke", (char *)"Blackstrap Path");
+    h.addItem( (char *)"apple", (char *)"Indean Meal Line");
+    h.addItem( (char *)"orange", (char *)"Larkhall Street");
+    h.addItem( (char *)"grape", (char *)"Outer Battery Road");
+    h.addItem( (char *)"salt", (char *)"Newfoundland Drive");
+    h.addItem( (char *)"pepper", (char *)"Baker Street");
+    h.addItem( (char *)"abc", (char *)"1");
+    h.addItem( (char *)"cab", (char *)"2");
+    h.addItem( (char *)"bac", (char *)"3");
+    h.addItem( (char *)"red", (char *)"1");
+    h.addItem( (char *)"blue", (char *)"2");
+    h.addItem( (char *)"yellow", (char *)"3");
+    h.addItem( (char *)"green", (char *)"1");
+    h.addItem( (char *)"purple", (char *)"2");
+    h.addItem( (char *)"white", (char *)"3");
 
-    h.addItem( (char *)"listen", (char *)"Newfoundland Drive");
-    h.addItem( (char *)"silent", (char *)"Baker Street");
+
 
     h.print_table();
+    h.printDistribution();
 
     return 0;
 }
