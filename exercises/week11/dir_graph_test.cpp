7dcae399722c1feb9abcbef4125b70cdedf0fc6f
GracePearcey <pearcey.grace@gmail.com>
2023-08-07 22:21:16 -0230

    added topological sort for directed weighted graph

---
+++ exercises/week11/dir_graph_test.cpp
@@ -0,0 +1,46 @@
+#include "directed_graph.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /**
+     * Weighted, Directed Graph
+    */
+    Graph<string,float> g;
+
+    int BB = g.addVertex("BB");
+    int CBS = g.addVertex("CBS");
+    int G = g.addVertex("G");
+    int H = g.addVertex("H");
+    int P = g.addVertex("P");
+    int PC = g.addVertex("PC");
+    int SJ = g.addVertex("SJ");
+
+    g.addEdge(PC,SJ,14.2);
+    g.addEdge(P,SJ,18.6);
+    g.addEdge(G,SJ,14.3);
+    g.addEdge(CBS,PC,16.4);
+    g.addEdge(P,PC,12.5);
+    g.addEdge(P,G,17.5);
+    g.addEdge(BB,G,15.3);
+    g.addEdge(H,BB,31.9);
+    g.addEdge(H,CBS,24.1);
+    g.addEdge(CBS,P,6.6);
+
+
+    //std::vector<float> d = g.shortestDijkstras(H, );
+//
+    //for (auto i : d)
+    //{
+    //    cout << i;
+    //}
+    //cout << endl;
+    //g.printDistanceMatrixWeighted();
+
+    g.topoSort();
+
+    return 0;
+};
--- exercises/week11/directed_graph.h
+++ exercises/week11/directed_graph.h
@@ -8,7 +8,7 @@
 using namespace std;
 template<typename T, typename E>
 /*
-* Undirected Graph
+* Directed Graph
 
 https://www.interviewcake.com/concept/java/topological-sort
 */
@@ -40,35 +40,25 @@ public:
     std::vector<E> distance_;
     std::vector<std::vector<Vertex<T>>> neighbors_;
     std::vector<Vertex<T>> path_;
+    std::vector<int> in_degs_; // in degree of each vertex
 
 
     size_t addVertex(T value)   
     {
         Vertex<T> new_vertex(std::forward<T>(value), this->verticies_.size());
         verticies_.push_back(new_vertex);
+        in_degs_.push_back(0);
         neighbors_.resize(verticies_.size());
         return new_vertex.getID();
     };
 
     void addEdge(size_t v1, size_t v2, E distance)
     {
-        Vertex<T> v1_copy = verticies_[v1];
-        v1_copy.distance_ = distance;
+        in_degs_[v2]++;
         Vertex<T> v2_copy = verticies_[v2];
         v2_copy.distance_ = distance;
         neighbors_[v1].push_back(v2_copy);
-        neighbors_[v2].push_back(v1_copy);
-        return;
-    }
-
-    void addEdge(size_t v1, size_t v2)
-    {
-        Vertex<T> v1_copy = verticies_[v1];
-        v1_copy.distance_ = 1;
-        Vertex<T> v2_copy = verticies_[v2];
-        v2_copy.distance_ = 1;
-        neighbors_[v1].push_back(v2_copy);
-        neighbors_[v2].push_back(v1_copy);
+        cout << "in degree of " << verticies_[v2].element_ << " is " << in_degs_[v2] << endl;
         return;
     }
 
@@ -134,10 +124,10 @@ public:
         return distance;
     }
 
-    bool verticiesDone(std::vector<bool> &vec)
+    bool verticiesDone(std::vector<bool> &vec, int value_that_means_its_not_done)
     {
         for (auto i : vec) {
-            if (i == false)
+            if (i == value_that_means_its_not_done)
             {
                 return false;
             }
@@ -146,42 +136,59 @@ public:
 
         return true;
     }
-    std::vector<E> shortestDijkstras(size_t source)
+    std::vector<E> shortestDijkstras(size_t source,std::vector<size_t>& path )
     {
         std::vector<E> distance(verticies_.size(), INFINITY);
-        std::vector<size_t> path(verticies_.size(), 0);
+        //std::vector<size_t> path(verticies_.size(), 0);
         std::vector<bool> done(verticies_.size(), false);
         distance[source] = 0;
-        Vertex<E> v = verticies_[0];
-        while (!verticiesDone(done))
+        Vertex<T> v = verticies_[0];
+        while (!verticiesDone(done,0))
         {
             //cout << "verticies not done" << endl;
             
+            // pick next vertex to work on
+            // get a comparison vertex that's not done:
+            while (done[v.getID()])
+            {
+                if (v.getID() == (verticies_.size()-1))
+                {
+                    v = verticies_[0];
+                }
+                else
+                {
+                    v = verticies_[v.getID() + 1];
+                } 
+            }
             for (auto i : this->verticies_)
             {
                 //cout << "checking if i is smaller" << endl;
                 if ((distance[i.getID()] < distance[v.getID()]) && (done[i.getID()] == false))
                 {
                     v = i;
-                    cout << "updating v" << endl;
+                    //cout << "updating v, v is " << v.element_ << endl;
+
                 }
             }
 
             //cout << "element value: " << v.getValue() << endl;
-            // TODO set all n.done to false, then set true
             for (auto n : neighbors_[v.getID()])
             {
                 E weight = n.distance_;
-                cout << "weight " << weight << endl;
+                if (weight != 15.3) 
+                {
+                    //cout << "weight " << weight << endl;
+                }
 
                 if (!done[n.getID()])
                 {
                     E dist = distance[v.getID()] + n.distance_;
-                    cout << "dist " << dist << endl;
+                    //cout << "dist " << dist << endl;
                     if (distance[n.getID()] > dist)
                     {
                         distance[n.getID()] = dist;
                         path[n.getID()] = v.getID();
+                        //cout << "updated: " << v.element_ << "'s neighbor " << n.element_  << " with total dist " << dist << endl;
                     }
                 }
 
@@ -218,12 +225,19 @@ public:
     }
     void printDistanceMatrixWeighted() {
         std::vector<std::vector<E>> matrix;
-        
+        std::vector<size_t> path(verticies_.size(), 0);
         // Create Matrix
         for (int i = 0; i < verticies_.size(); i++)
         {
-            matrix.push_back(shortestDijkstras(i));
+            matrix.push_back(shortestDijkstras(i, path));
         }
+
+        // Print Elements
+        for (int j = 0; j < path.size(); j++)
+        {
+            cout << path[j] << " ";
+        }
+        cout << endl;
 
         // Print Matrix        
         for (int j = 0; j < verticies_.size(); j++)
@@ -236,7 +250,67 @@ public:
         }
 
     }
+    bool verticiesDoneTopo(std::vector<bool> &vec, int value_that_means_its_done)
+    {
+        for (auto i : vec) {
+            if (i != value_that_means_its_done)
+            {
+                return false;
+            }
 
+        }
+
+        return true;
+    }
+    void topoSort()
+    {
+        vector<int> degrees_copy = this->in_degs_;
+        //vector<size_t> finished;
+        queue<size_t> worklist;
+
+        // find a vertex with in degree 0
+        Vertex<T> v = verticies_[0];
+        while(degrees_copy[v.getID()] != 0)
+        {
+            if (v.getID() == verticies_.size())
+            {
+                cout << " No verticies with in degree of 0, can't do topological sort" << endl;
+                return;
+            }
+            v = verticies_[v.getID() + 1];
+            
+        }
+
+        // v has in-degree of 0, so no predessesors, so we can add it to the worklist
+        worklist.push(v.getID());
+
+        cout << "Topo Sort: ";
+        while (!worklist.empty())
+        {
+            size_t current_v = worklist.front();
+
+            // decrement the in degree of it's neighbors, and add them to the worklist if their in degree is now 0
+            for (auto n: neighbors_[current_v])
+            {
+                degrees_copy[n.getID()]--;
+                if (degrees_copy[n.getID()] == 0)
+                {
+                    worklist.push(n.getID());
+                }
+            }
+
+            // visit the current vertex
+            cout << verticies_[current_v].element_ << ", " ;
+            // remove the current vertex from the worklist, it's done now
+            worklist.pop();
+
+        }
+        cout << endl;
+
+
+
+
+    }
 
     ~Graph() {}
     
